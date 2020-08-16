ee8f885251198ce65aecd2966fe80ff4dc07f836
taha <tahaatrian30@gmail.com>
2020-08-16 11:22:05 +0430

    Exp model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Exp(models.Model):
+    text = models.CharField(max_length=260)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default="")
+
+
