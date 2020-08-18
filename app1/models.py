7c2a004f37ff743edaff6a019a4c156eaa31f6e6
taha <tahaatrian30@gmail.com>
2020-08-18 13:56:43 +0430

    income model and gitignore update

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,6 +8,17 @@ class Exp(models.Model):
     text = models.CharField(max_length=260)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
+
+    def __str__(self):
+        return "{}-{}".format(self.amount, self.date)
 
 
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
+
+    def __unicode__(self):
+        return "{}-{}".format(self.amount, self.date)
