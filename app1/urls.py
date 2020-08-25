d23c7732c558ccda6ece4e5c5b761ae7f26fceef
taha <tahaatrian30@gmail.com>
2020-08-25 10:26:16 +0430

    income submit

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
-
 urlpatterns = [
-    url(r'^submit/exp/$', views.submit_exp, name='submit_exp')
+    url(r'^submit/exp/$', views.submit_exp, name='submit_exp'),
+    url(r'^submit/income/$', views.submit_income, name='submit_income')
 ]
+
--- app1/views.py
+++ app1/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-from .models import Exp,Token,Income,User
+from .models import Exp, Token, Income, User
 from datetime import datetime
+
+
+@csrf_exempt
+def submit_income(request):
+    """user submit an income"""
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+
+    Income.objects.create(user=this_user, amount=request.POST['amount'],
+                          text=request.POST['text'], date=date)
+    return JsonResponse({
+        'status ': 'ok',
+    }, encoder=json.JSONEncoder)
 
 
 # Create your views here.
